c40db475620f326660bd801c021f9cbdfd0240ad
Mike Herchel <mike@herchel.com>
2017-08-26 14:31:56 -0400

    Work on organization component.

--- src/modules/HomePage/Organization/Organization.jsx
+++ src/modules/HomePage/Organization/Organization.jsx
@@ -3,15 +3,22 @@ import React from 'react';
 const Organization = (props) => {
   const { organization } = props;
   return (
-    <div>
-      <h1>{organization.name}</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </p>
-    </div>
+    <article className="organization">
+      <h2 className="organization__title">{organization.name}</h2>
+      <div class="organization__below">
+        <div class="organization__raised">
+          <span class="organization__raised--amount">$2,450</span>
+          <span class="organization__raised--text"> Raised</span>
+        </div>
+        <div class="organization__pledged">
+          <span class="organization__pledged--amount">$2,450</span>
+          <span class="organization__pledged--text"> Pledged by </span>
+          <span class="organization__pledged--count">127</span>
+          <span class="organization__pledged--unit"> People</span>
+        </div>
+      </div>
+      <button type="button">Pledge or Donate Now</button>
+    </article>
   )
 };
 
