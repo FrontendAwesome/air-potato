a78c7382c5f9a539738474be3dc6e487d354b73a
David Smith <david@startingdreams.com>
2017-08-26 11:25:45 -0400

    more power

---
+++ src/modules/HomePage/Raised/Raised.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Raised = () => (
+  <div>
+    <h1>Raised</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </p>
+  </div>
+);
+
+export default Raised;
