a890f052b51cb91dfa25dcd635ce47e60534ec83
Mike Herchel <mike@herchel.com>
2017-08-26 16:13:34 -0400

    Adding header and footer to static pages.

--- src/modules/About/About.jsx
+++ src/modules/About/About.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 const About = () => (
   <div>
+    <Header />
     <h1>About</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -9,6 +12,7 @@ const About = () => (
       Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
       Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
     </p>
+    <Footer />
   </div>
 );
 
