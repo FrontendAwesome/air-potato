83e77dc0dbf3abf1ad07ee654b969a001683fc10
David Smith <david@startingdreams.com>
2017-08-26 16:11:35 -0400

    transactions

--- src/modules/Donation/Donation.jsx
+++ src/modules/Donation/Donation.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getOrganization } from '../../ducks/organizations';
-
+import { getMetrics } from '../../ducks/metrics';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export class Donation extends React.Component {
   render() {
-    const { organization } = this.props;
+    const { organization, metrics } = this.props;
+    console.log('metrics', metrics);
     return (
       <div>
-        <h1>Donation for {organization.name}</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-        </p>
+        <Header />
+        <h1>{organization.name}</h1>
+        <section>
+          <p>Join {organization.total_transactions} other people who already pledged!</p>
+          <hr />
+          <ul>
+          { metrics.map((metric, key) => (
+            <li></li>
+          ))}
+          </ul>
+        </section>
+        <Footer />
       </div>
     )
   }
@@ -24,6 +32,7 @@ export class Donation extends React.Component {
 
 Donation.propTypes = {
   organization: PropTypes.shape({}).isRequired,
+  metrics: PropTypes.array.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
@@ -31,7 +40,8 @@ Donation.propTypes = {
 
 export default connect(
   (state, { match:{params:{organizationKey}} }) => ({
-      organization: getOrganization(state, organizationKey),
+    organization: getOrganization(state, organizationKey),
+    metrics: getMetrics(state),
   }),
   null,
 )(Donation);
