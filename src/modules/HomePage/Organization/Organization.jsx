fcc6799aa1292c6e8e74fa6055c92e7848e3b743
Mark Solters <msolters@gmail.com>
2017-08-26 15:43:54 -0400

    Organization component renders real data

--- src/modules/HomePage/Organization/Organization.jsx
+++ src/modules/HomePage/Organization/Organization.jsx
@@ -8,14 +8,22 @@ const Organization = (props) => {
       <h2 className="organization__title">{organization.name}</h2>
       <div className="organization__below">
         <div className="organization__raised">
-          <span className="organization__raised--amount">$2,450</span>
-          <span className="organization__raised--text"> Raised</span>
-        </div>
-        <div className="organization__pledged">
-          <span className="organization__pledged--amount">$2,450</span>
-          <span className="organization__pledged--text"> Pledged by </span>
-          <span className="organization__pledged--count">127</span>
+          <span className="organization__raised--amount">${organization.money_earned.total.toFixed(2)}</span>
+          <span className="organization__raised--text"> Raised by </span>
+          <span className="organization__pledged--count">{organization.total_transactions}</span>
           <span className="organization__pledged--unit"> People</span>
+        </div>
+      </div>
+      <div className="organization__below">
+        <div className="organization__pledged">
+          <span className="organization__pledged--amount">${organization.money_earned.pledges.toFixed(2)}</span>
+          <span className="organization__pledged--text"> Pledged / </span>
+          <span className="organization__pledged--amount">${organization.money_earned.donations.toFixed(2)}</span>
+          <span className="organization__pledged--text"> Donated</span>
+        </div>
+      </div>
+      <div className="organization__below">
+        <div className="organization__pledged">
         </div>
       </div>
       <button type="button" className="organization__donate">Pledge or Donate Now</button>
