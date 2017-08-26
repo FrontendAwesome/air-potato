acb2678ee5adf0ea3aa8e1386132fd053e77f158
Mike Herchel <mike@herchel.com>
2017-08-26 16:47:21 -0400

    General shit and stuff.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,7 @@ import './Footer.css';
 const Footer = () => (
   <footer className="footer">
     <div className="footer__inner">
-      Powered by an <a target="_blank" rel="noopener noreferrer" href="https://github.com/FrontendAwesome/air-potato">Air Potato</a>!
-      | <FirebaseAuthBar />
+      <FirebaseAuthBar /> &nbsp;&nbsp;|&nbsp;&nbsp; Powered by an <a target="_blank" rel="noopener noreferrer" href="https://github.com/FrontendAwesome/air-potato">Air Potato</a>!
     </div>
   </footer>
 );
