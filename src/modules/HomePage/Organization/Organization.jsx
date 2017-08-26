082d56f3b29de100ef19fd9059bfb63a6dd948a1
Mike Herchel <mike@herchel.com>
2017-08-26 14:33:13 -0400

    Classnames in organization component.

--- src/modules/HomePage/Organization/Organization.jsx
+++ src/modules/HomePage/Organization/Organization.jsx
@@ -5,16 +5,16 @@ const Organization = (props) => {
   return (
     <article className="organization">
       <h2 className="organization__title">{organization.name}</h2>
-      <div class="organization__below">
-        <div class="organization__raised">
-          <span class="organization__raised--amount">$2,450</span>
-          <span class="organization__raised--text"> Raised</span>
+      <div className="organization__below">
+        <div className="organization__raised">
+          <span className="organization__raised--amount">$2,450</span>
+          <span className="organization__raised--text"> Raised</span>
         </div>
-        <div class="organization__pledged">
-          <span class="organization__pledged--amount">$2,450</span>
-          <span class="organization__pledged--text"> Pledged by </span>
-          <span class="organization__pledged--count">127</span>
-          <span class="organization__pledged--unit"> People</span>
+        <div className="organization__pledged">
+          <span className="organization__pledged--amount">$2,450</span>
+          <span className="organization__pledged--text"> Pledged by </span>
+          <span className="organization__pledged--count">127</span>
+          <span className="organization__pledged--unit"> People</span>
         </div>
       </div>
       <button type="button">Pledge or Donate Now</button>
