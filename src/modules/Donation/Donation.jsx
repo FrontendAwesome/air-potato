7c38c2b16b9a0fb1d2880bbca2a65ba28b19c528
Mike Herchel <mike@herchel.com>
2017-08-26 16:36:08 -0400

    Work on styling donations page.

--- src/modules/Donation/Donation.jsx
+++ src/modules/Donation/Donation.jsx
@@ -5,6 +5,7 @@ import { getOrganization } from '../../ducks/organizations';
 import { getMetrics } from '../../ducks/metrics';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import './Donation.css';
 
 export class Donation extends React.Component {
   render() {
@@ -18,23 +19,25 @@ export class Donation extends React.Component {
           <p>Join {organization.total_transactions} other people who already pledged!</p>
           <hr />
           { metrics.map((metric, key) => (
-            <div>
-              $<input type="text" className="metric-item__input" />
-              <div className="metric-item__label">
-                <div className="metric-item__text">per tiki torch counted</div>
-                <div className="metric-item__suggested">$1.00 Suggested</div>
+            <div className="donation-metric">
+              <span className="donation-metric__dollar">$</span><input type="text" className="donation-metric__input" />
+              <div className="donation-metric__label">
+                <div className="donation-metric__text">per tiki torch counted</div>
+                <div className="donation-metric__suggested">$1.00 Suggested</div>
               </div>
             </div>
           ))}
         </section>
-        <section className="donate__fixed">
-          $<input type="text" className="metric-item__input" />
-            <div className="metric-item__label">
-              <div className="metric-item__text">Donate Fixed Amount</div>
-              <div className="metric-item__suggested">$1.00 Suggested</div>
+        <section className="donation__fixed">
+          <div className="donation-metric">
+            <span className="donation-metric__dollar">$</span><input type="text" className="donation-metric__input" />
+              <div className="donation-metric__label">
+                <div className="donation-metric__text">Donate Fixed Amount</div>
+                <div className="donation-metric__suggested">$1.00 Suggested</div>
+              </div>
             </div>
         </section>
-        <button className="donate__donate-now">Donate Now!</button>
+        <button className="donation__donate-now">Donate Now!</button>
         <Footer />
       </div>
     )
