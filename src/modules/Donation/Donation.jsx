fc7dcec3459a4cde646ae189ee89fd58bd5967e0
David Smith <david@startingdreams.com>
2017-08-26 16:41:48 -0400

    almost

--- src/modules/Donation/Donation.jsx
+++ src/modules/Donation/Donation.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { getOrganization } from '../../ducks/organizations';
 import { getMetrics } from '../../ducks/metrics';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import './Donation.css';
 
+
 export class Donation extends React.Component {
+  submitForm = () => {
+    const { history } = this.props;
+    const transaction = {};
+    transaction.test = document.getElementById('fixed-donation');
+    console.log('trans', transaction);
+    history.push(`/`);
+  };
+
   render() {
     const { organization, metrics } = this.props;
-    console.log('metrics', metrics);
     return (
       <div>
         <Header />
@@ -19,8 +28,8 @@ export class Donation extends React.Component {
           <p>Join {organization.total_transactions} other people who already pledged!</p>
           <hr />
           { metrics.map((metric, key) => (
-            <div className="donation-metric">
-              <span className="donation-metric__dollar">$</span><input type="text" className="donation-metric__input" />
+            <div className="donation-metric" key={key}>
+              <span className="donation-metric__dollar">$</span><input type="text" className="donation-metric__input" name={key} id={key} />
               <div className="donation-metric__label">
                 <div className="donation-metric__text">per tiki torch counted</div>
                 <div className="donation-metric__suggested">$1.00 Suggested</div>
@@ -30,20 +39,19 @@ export class Donation extends React.Component {
         </section>
         <section className="donation__fixed">
           <div className="donation-metric">
-            <span className="donation-metric__dollar">$</span><input type="text" className="donation-metric__input" />
+            <span className="donation-metric__dollar">$</span><input type="text" className="donation-metric__input" name="fixed-donation" id="fixed-donation" />
               <div className="donation-metric__label">
                 <div className="donation-metric__text">Donate Fixed Amount</div>
                 <div className="donation-metric__suggested">$1.00 Suggested</div>
               </div>
             </div>
         </section>
-        <button className="donation__donate-now">Donate Now!</button>
+        <button className="donate__donate-now" onClick={this.submitForm}>Donate Now!</button>
         <Footer />
       </div>
     )
   }
 };
-
 
 Donation.propTypes = {
   organization: PropTypes.shape({}).isRequired,
@@ -53,10 +61,10 @@ Donation.propTypes = {
   }).isRequired,
 };
 
-export default connect(
+export default withRouter(connect(
   (state, { match:{params:{organizationKey}} }) => ({
     organization: getOrganization(state, organizationKey),
     metrics: getMetrics(state),
   }),
   null,
-)(Donation);
+)(Donation));
