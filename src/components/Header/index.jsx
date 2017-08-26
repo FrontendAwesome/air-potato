5d9e25e7326b90f95d03545ba004e090afcf7f60
David Smith <david@startingdreams.com>
2017-08-26 11:01:57 -0400

    head and footer

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (
+    <div>Fiiter</div>
+);
+
+export default Footer;
--- src/modules/Root/Root.jsx
+++ src/modules/Root/Root.jsx
@@ -11,9 +11,6 @@ import LiveResults from '../LiveResults';
 import HomePage from '../HomePage';
 import Admin from '../Admin';
 import NoMatch from '../NoMatch';
-import ExampleRedux from '../Examples/ExampleRedux';
-import ExampleDatabase from '../Examples/ExampleDatabase';
-import logo from './logo.svg';
 import './Root.css';
 
 injectTapEventPlugin();
@@ -25,23 +22,15 @@ export default function Root() {
       <BrowserRouter>
         <div id="root">
           <FirebaseInit />
-          <div className="app-header">
-            <img src={logo} className="app-logo" alt="logo" style={{maxWidth: '200px'}} />
-            <h2>Welcome to React</h2>
-          </div>
           <div>
             <Switch>
               <Route exact path="/" component={HomePage} />
-              <Route exact path="/liverestuls" component={LiveResults} />
+              <Route exact path="/liveresults" component={LiveResults} />
               <PrivateRoute exact path="/admin" component={Admin} />
               <Route component={NoMatch} />
             </Switch>
           </div>
           <FirebaseAuthBar />
-          <hr />
-          <h1>Examples</h1>
-          <ExampleRedux />
-          <ExampleDatabase />
         </div>
       </BrowserRouter>
     </Provider>
